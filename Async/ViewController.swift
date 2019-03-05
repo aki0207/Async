c02a6394a4dc849dba18e6d2f8d1fc9d6b33a47d
Akihiro Nagami <nagami@kikyos.co.jp>
2019-03-05 12:42:08 +0900

    非同期処理完全に理解した

--- Async/ViewController.swift
+++ Async/ViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ActivityIndicator: UIActivityIndicatorView!
+    var result:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ActivityIndicatorを作成＆中央に配置
+        ActivityIndicator = UIActivityIndicatorView()
+        ActivityIndicator.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        ActivityIndicator.center = self.view.center
+        
+        // クルクルをストップした時に非表示する
+        ActivityIndicator.hidesWhenStopped = true
+        // 色を設定
+        ActivityIndicator.style = UIActivityIndicatorView.Style.gray
+        //Viewに追加
+        self.view.addSubview(ActivityIndicator)
+        // クルクルスタート
+        ActivityIndicator.startAnimating()
+        
+        
+        
+        
+        DispatchQueue.global().async {
+            // グローバルキューで実行される
+            for i in 0..<5 {
+                print(i)
+                Thread.sleep(forTimeInterval: 1.0)
+                if i == 4 {
+                    self.result = i
+                }
+                
+            }
+            
+            DispatchQueue.main.async {
+                // メインキューで実行される
+                // クルクルストップ
+                self.ActivityIndicator.stopAnimating()
+                let label = UILabel()
+                label.frame = CGRect(x:150, y:200, width:160, height:30)
+                label.text = "結果は" + String(self.result)
+                self.view.addSubview(label)
+            }
+        }
     }
+    
+  
+    
+
 
 
 }
